8cceb8c9711972056f994a74069b543ff9cdd9d3
Oscar <43421688+dert98@users.noreply.github.com>
2021-04-10 15:13:35 -0300

    see products
    se cen los productos json y vue funcionan

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="icon" type="image/svg" href="img/icon.svg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://dert98.github.io/Porfolio/global.css">
@@ -16,8 +15,30 @@
 <body>
     <header>
     </header>
-    <main id="root">
-        <?php include("productos.php"); ?>
+    <main>
+        <div id="app">
+            <div class="container text-center m-auto">
+                <div class="text-center">
+                    <div class="m-4">
+                        <label class="h1">{{list['name']}}</label>
+                    </div>
+                    <div class="col-md-12 row">
+                        <div v-for="producto in list['productos']" class="col-md-6 mb-4">
+                            <p>
+                                <!-- <img v-bind:src="img['src']" class="cp2 b4" alt=""> -->
+                            </p>
+                            <p>
+                                <label for="" class="">{{producto['nombre']}}</label>
+                            </p>
+                            <p class="text-streng">
+                                <a href="" class="btn btn-success">ver</a>
+                                <a href="" class="btn btn-success">comentar</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
     <script src="js/app.js"></script>
     <!-- jQuery library -->
--- js/app.js
+++ js/app.js
@@ -1,27 +1,14 @@
 var app = new Vue({
   el: "#app",
   data: {
-    list: null,
-    usuarios: []
+    list: {},
   },
   
   mounted: function () {
-    this.getAllusuarios();
     this.getUsers();
   },
   
   methods: {
-      getAllusuarios: function () {
-        axios.get("../php/api.php?action=read")
-        .then(function (response) {
-          if (response.data.error) {
-            app.errorMessage = response.data.message;
-          } else {
-            app.usuarios = response.data.usuarios;
-            app.filtrados = response.data.usuarios;
-          }
-        })
-    },
     getUsers: function() {
 			var lista = this;
 			axios.get("./config.json").then(function(response) {
@@ -31,19 +18,5 @@ var app = new Vue({
 			});
 		}
   },
-  computed: {
-      getTotalusuarios: function () {
-        return this.Usuarios.length;
-      },
-  
-      UsuariosVacio: function () {			
-        return this.getTotalUsuarios == 0;
-      },
-  
-      busqueda: function () {
-          return this.filtrados.filter((item) => item.busqueda.includes(this.busqueda));
-      }
-  
-    },
   })
     
